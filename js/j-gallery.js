10ad80d5fb13597338dab2e29bec2e7b1d531342
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-08-28 13:39:34 +0100

    Added basic bootstrap image gallery using a carousel. Found on bootply.

--- js/j-gallery.js
+++ js/j-gallery.js
@@ -1,79 +1,19 @@
-var gallery;
-var numOfSlides;
-var currentPos;
 
-function start()
-{
-    gallery = document.getElementById('j-gallery');
-    numOfSlides = 0;
-    currentPos = 0;
 
-    getNumOfSlides();
-    deactivateImages();
+// handles the carousel thumbnails
+$('[id^=carousel-selector-]').click( function(){
+  var id_selector = $(this).attr("id");
+  var id = id_selector.substr(id_selector.length -1);
+  id = parseInt(id);
+  $('#myCarousel').carousel(id);
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $(this).addClass('selected');
+});
 
-    document.getElementById('j-prev').addEventListener('click', function(){changePos("prev")});
-    document.getElementById('j-next').addEventListener('click', function(){changePos("next")});
-}
-
-function getNumOfSlides()
-{
-    let classes = gallery.className.split(/\s+/);
-    numOfSlides = parseInt(classes[0].replace(classes[0].slice(0, 10), ''));
-}
-
-function deactivateImages()
-{
-    document.getElementById(0).style.display = "inline";
-    for (var i = 1; i < numOfSlides; i++)
-        {
-            document.getElementById(i).style.display = "none";
-        }
-}
-
-function changePos(dir)
-{
-    if (dir === "next")
-        {
-            if (currentPos < numOfSlides - 1)
-                {
-                    currentPos++;
-                }
-            else
-                {
-                    currentPos = 0;
-                }
-        }
-    else if (dir === "prev")
-        {
-            if (currentPos > 0)
-                {
-                    currentPos--;
-                }
-            else
-                {
-                    currentPos = numOfSlides - 1;
-                }
-        }
-
-    showActiveImage();
-}
-
-function showActiveImage()
-{
-    document.getElementById(currentPos).style.display = "inline";
-
-    for (var i = 0; i < numOfSlides; i++)
-        {
-            if (i === currentPos){}
-            else
-                {
-                    document.getElementById(i).style.display = "none";
-                }
-            
-        }
-}
-
-window.onload = function()
-{
-    start();
-}
+// when the carousel slides, auto update
+$('#myCarousel').on('slid', function (e) {
+  var id = $('.item.active').data('slide-number');
+  id = parseInt(id);
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $('[id=carousel-selector-'+id+']').addClass('selected');
+});
