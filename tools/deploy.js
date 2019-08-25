40ebd5a2a9d24521cb3ec30797daa2badd24674a
Pjaerr <joshcharlesjackson@gmail.com>
2019-08-25 17:09:19 +0100

    Starting "deploy" script

---
+++ tools/deploy.js
@@ -0,0 +1,9 @@
+/**
+ * * Copy contents of dist/ folder
+ * * Checkout gh-pages branch
+ * * Delete all files in the directory excluding CNAME and .gitignore
+ * * Paste contents of dist/ folder
+ * * Stage all changes
+ * * Commit all changes as Current Date
+ * * Push Changes to remote
+ */
