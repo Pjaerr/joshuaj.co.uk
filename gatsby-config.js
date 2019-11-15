077c66b0887ec5590bc216e26fabf4c4a7b3b1b0
Pjaerr <joshcharlesjackson@gmail.com>
2019-11-15 23:07:51 +0000

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -111,5 +111,21 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146439931-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
   ],
 }
