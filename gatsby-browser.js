4eab728633d4af6ed3baf72215deff92c33eefea
Pjaerr <joshcharlesjackson@gmail.com>
2020-04-17 19:36:27 +0100

    Wrap contents of all <li> elements in a div
    Fixes issue with layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,11 @@ import Image from "./src/components/blog/Image/Image";
 
 const components = {
   img: Image,
+  li: props => (
+    <li {...props}>
+      <div>{props.children}</div>
+    </li>
+  ),
 };
 
 export const wrapRootElement = ({ element }) => (
