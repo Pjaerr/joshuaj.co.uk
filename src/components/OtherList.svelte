9a0f0ebc3f6866d925bf90ce7be25004324dec4d
Joshua Jackson <joshcharlesjackson@gmail.com>
2023-02-05 13:58:54 +0000

    Link to blender stuff on homepage
    Duplicating styles here as I plan on restructuring everything sometime in the future so would be wasted effort right now

---
+++ src/components/OtherList.svelte
@@ -0,0 +1,38 @@
+<ul>
+  <li>
+    <h3>
+      <a href="/blender"> Blender Projects </a>
+    </h3>
+
+    <p>
+      A collection of things made using Blender each month in an effort to learn
+      3D modelling
+    </p>
+  </li>
+</ul>
+
+<style>
+  ul {
+    display: grid;
+    padding: 0;
+    padding-top: 48px;
+    grid-template-columns: auto;
+    grid-gap: 48px;
+  }
+
+  /* Desktop */
+  @media only screen and (min-width: 1048px) {
+    ul {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  li {
+    max-width: 550px;
+    list-style: none;
+  }
+</style>
