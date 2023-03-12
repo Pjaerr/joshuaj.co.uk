f5e7fb29c12c9640c8231d746f4e652d8751ee9f
Joshua Jackson <joshcharlesjackson@gmail.com>
2023-03-12 20:46:09 +0000

    fixup! Use Astro Content Collections and add support for 'pinned' blogposts

--- src/utils/sort-by-date.js
+++ src/utils/sort-by-date.js
@@ -1,21 +1,5 @@
 import { parseISO } from "date-fns";
 
-/**
- * Sorts an array of items with a `publishDate` property
- * @param {{ publishDate }[]} posts
- * @param {'desc' | 'asc'} order
- */
-export default function (posts, order = "desc") {
-  return posts.sort((a, b) => {
-    if (order === "desc") {
-      return (
-        parseISO(b.frontmatter.publishDate) -
-        parseISO(a.frontmatter.publishDate)
-      );
-    }
-
-    return (
-      parseISO(a.frontmatter.publishDate) - parseISO(b.frontmatter.publishDate)
-    );
-  });
+export default function (a, b) {
+  return parseISO(b.data.publishDate) - parseISO(a.data.publishDate);
 }
